657830f722b7e0249b1f4c93f9ae8574c67ca955
Kiss Mate <km.kissmate@gmail.com>
2025-05-18 19:12:31 +0200

    hosting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
+import { provideAnimations } from '@angular/platform-browser/animations'; 
 import { provideHttpClient } from '@angular/common/http'; 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -10,7 +10,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
-    provideAnimationsAsync(), 
+    provideAnimations(), 
     provideHttpClient(), provideFirebaseApp(() => initializeApp({ projectId: "zoldseg-17662", appId: "1:698457405340:web:ef8389de531d584f39fa59", storageBucket: "zoldseg-17662.firebasestorage.app", apiKey: "AIzaSyCMMTe9hrxp1qsXcAVkHK-ItiTUY21H6WY", authDomain: "zoldseg-17662.firebaseapp.com", messagingSenderId: "698457405340" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()) 
   ]
 };
